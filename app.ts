d411463a134b4e04f89ff01659d95d17b425fc40
Ivan_WEB <ivan.exceedteam@gmail.com>
2020-12-07 10:36:52 +0300

    Create server(app.js) and create middleware(auth)

--- app.ts
+++ app.ts
@@ -1,1 +1,36 @@
 //start server 
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const todoitem = require('./routes/todoitem.route')
+const user = require('./routes/user.route')
+const app = express()
+const mongoose = require('mongoose')
+const dev_db_url = `mongodb+srv://${process.env.USER_MONGO}:${process.env.PASSWORD_USER_MONGO}@cluster0.lxer2.mongodb.net/${process.env.NAME_DATABASE}?retryWrites=true&w=majority`
+let mongooseOptions = { useNewUrlParser: true, useUnifiedTopology: true }
+mongoose.connect(dev_db_url, mongooseOptions)
+mongoose.Promise = global.Promise
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*')
+	res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With')
+	next()
+})
+
+app.use('/api/todoitem', todoitem)
+app.use('/api/user', user)
+
+app.use('/', express.static(path.join(__dirname, '../../build/')))
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname + '/../../build/index.html'))
+})
+
+app.listen(process.env.PORT, () => {
+	console.log('Server is up and running on port numner ' + process.env.PORT)
+})
---
+++ middleware/auth.middleware.ts
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+	try {
+		const token = req.headers.authorization.split(' ')[1] // "Bearer TOKEN"
+
+		if (!token) {
+			return res.status(401).json({ message: 'Нет авторизации' })
+		}
+
+		const decoded = jwt.verify(token, process.env.JWT_SECRET)
+		req.user = decoded
+		next()
+	} catch (e) {
+		res.status(401).json({ message: 'Нет авторизации' })
+	}
+}
